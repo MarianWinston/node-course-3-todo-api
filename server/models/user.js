20f103f0b023f84a4673a35ffd1561cd2ff0a187
Marian Jitaru <marian.jitaru3@gmail.com>
2019-01-03 04:22:39 +0000

    Add POST /users route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,57 @@
-let mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-let User = mongoose.model('User', {
+let UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
         minlenght: 1,
-        trim: true
-    }
+        trim: true,
+        unique: true,      // no same email
+        // email validation using a validator library
+        validate: {
+            validator: validator.isEmail, 
+            message: '{VALUE} is not a value email'
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlenght: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
+
+UserSchema.methods.toJSON = function () {
+    let user = this;
+    let userObject = user.toObject();
+
+    return _.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function () {
+    let user = this;
+    let access = 'auth';
+    let token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+    user.tokens = user.tokens.concat([{access, token}]); // pushes access and token to array tokens
+
+    return user.save().then(() => {
+        return token;
+    });
+};  
+
+let User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
